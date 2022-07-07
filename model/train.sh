4c952fd0df2a06ab505c9ff3d4341dbf127eb7b1
Katinskaia Anisia <anisia.katinskaia@helsinki.fi>
2022-07-07 09:39:54 +0300

    Mods

--- model/train.sh
+++ model/train.sh
@@ -1,4 +1,21 @@
-python train.py --outdir ../checkpoints/bert_model \
---train_path ../data/train \
---valid_path ../data/dev \
---bert_pretrain bert-base-cased
+#!/usr/bin/env bash
+
+#SBATCH --account=project_2002016
+#SBATCH --job-name=vernet
+
+#SBATCH --partition=gpu
+
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=80G
+#SBATCH --ntasks=1
+#SBATCH --time=2:00:00
+#SBATCH --gres=gpu:v100:1
+
+module load gcc/8.3.0 cuda/10.1.168
+
+export DIR=/projappl/project_2002016/VERNet
+cd $DIR
+
+conda activate vernet
+
+srun python3 ./train.py --outdir ../vernet_model --train_path /scratch/project_2002016/datasets/data-gec/vernet_data/vtrain --valid_path /scratch/project_2002016/datasets/data-gec/vernet_data/vvalid --bert_pretrain sberbank-ai/ruBert-base
