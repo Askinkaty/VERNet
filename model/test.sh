c7bd7b121b1a3d546c59d21381667b4b7a9e0293
Katinskaia Anisia <anisia.katinskaia@helsinki.fi>
2022-07-08 16:08:21 +0300

    Mods

--- model/test.sh
+++ model/test.sh
@@ -20,5 +20,5 @@ conda activate gpt
 
 #srun python3 ./test.py --test_path /scratch/project_2002016/datasets/data-gec/vernet_data/test.jsonl --bert_pretrain sberbank-ai/ruBert-base --checkpoint ../vernet_model/model.best.pt
 # {'p': 0.9053114916852817, 'r': 0.544931650108314, 'f': 0.6803450687806015, 'f05': 0.7995572020430084}
-srun python3 ./test_src.py --test_path /scratch/project_2002016/datasets/data-gec/vernet_data/test.jsonl --bert_pretrain sberbank-ai/ruBert-base --checkpoint ../vernet_model/model.best.pt
-
+srun python3 ./test_src.py --test_path /scratch/project_2002016/datasets/data-gec/vernet_data/test.jsonl --bert_pretrain sberbank-ai/ruBert-large --checkpoint ../vernet_model/model.best.pt
+#{'p': 0.7822736030828517, 'r': 0.15687789799072643, 'f': 0.261345349211458, 'f05': 0.43524871355060035}
